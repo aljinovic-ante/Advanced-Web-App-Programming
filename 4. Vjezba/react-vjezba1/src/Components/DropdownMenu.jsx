7a15c27557e5c108f3402b5288fab6ad88e52392
Ante Aljinović <antealjinovic111@gmail.com>
2024-11-18 14:06:57 +0100

    4. vjezba fix

--- 4. Vjezba/react-vjezba1/src/Components/DropdownMenu.jsx
+++ 4. Vjezba/react-vjezba1/src/Components/DropdownMenu.jsx
@@ -1,7 +1,9 @@
 export default function DropdownMenu({options,value,onChange}){
     return (
     <select value={value} onChange={(e)=>{console.log("odabrano: ", e.target.value);onChange(e.target.value)}}>
-    {   options.map((item, index)=>(<option value={item} key={index}>{item}</option>))}
+    {   options.map((item, index)=>(
+            <option value={item} key={index}>{item}</option>
+        ))}
     </select>
 )
 }
