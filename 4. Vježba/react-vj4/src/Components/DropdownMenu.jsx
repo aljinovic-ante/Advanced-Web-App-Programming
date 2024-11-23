667d16a89570dce9e3585e7b3ffe49c127de42da
Ante <antealjinovic111@gmail.com>
2024-11-23 13:51:12 +0100

    fix 4. vj

--- 4. Vježba/react-vj4/src/Components/DropdownMenu.jsx
+++ 4. Vježba/react-vj4/src/Components/DropdownMenu.jsx
@@ -1,6 +1,6 @@
 export default function DropdownMenu({options,value,onChange}){
     return (
-    <select value={value} onChange={(e)=>{console.log("odabrano: ", e.target.value);onChange(e.target.value)}}>
+    <select value={value} onChange={(e)=>{onChange(e.target.value)}}>
     {   options.map((item, index)=>(<option value={item} key={index}>{item}</option>))}
     </select>
 )
